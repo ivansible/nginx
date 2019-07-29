3268d14440d61cef329e09263bcfca6a91dada4f
Ivan Andreev <ivandeex@gmail.com>
2019-07-29 23:54:00 +0300

    lin-sslh: replace filesystem capabilities by systemd
    also enable:
    - non-root mode (configurable, fails on openvz)
    - http redirection (configurable)
    - replace fork -> select in ssh redirector (configurable)

--- roles/lin-sslh/files/iptables-helper.sh
+++ roles/lin-sslh/files/iptables-helper.sh
@@ -2,107 +2,94 @@
 set -euo pipefail
 #set -x
 
-. /etc/default/sslh
-ACTION="${1:-}"
-SSLH_UID=sslh
-COMMENT=sslh_helper_ruleset
-TAG="-m comment --comment ${COMMENT}"
+#. /etc/default/sslh
+case "${USE_TRANSPARENT:-}" in
+  true)  ;;
+  false) exit 0 ;;
+  *) echo "$0 must be run by systemd"; exit 1 ;;
+esac
 
-set_capabilities()
+rules()
 {
-  case ${1} in
-    enable)
-      CAPS="cap_net_bind_service,cap_net_admin+ep"
-      /sbin/setcap ${CAPS} /usr/sbin/sslh-select
-      /sbin/setcap ${CAPS} /usr/sbin/sslh
+  case "$1" in
+    start)
+      OPT="-A"
+      CMD="add"
+      LOG=""
+      SYSCTL=true
       ;;
-    disable)
-      /sbin/setcap "" /usr/sbin/sslh-select
-      /sbin/setcap "" /usr/sbin/sslh
-      ;;
-    ignore)
-      ;;
-    *)
-      echo "invalid value of TRANSPARENT_SETCAP"
-      exit 1
+    stop)
+      OPT="-D"
+      CMD="del"
+      LOG="2>/dev/null || true"  # this hack requires eval
+      SYSCTL=false
       ;;
   esac
-}
 
-start()
-{
-  [ ${USE_TRANSPARENT} = true ] || return
+  if [ ${SYSCTL} = true ]; then
+    # Allow sslh to use localhost as destination
+    /sbin/sysctl -q -w net.ipv4.conf.default.route_localnet=1
+    /sbin/sysctl -q -w net.ipv4.conf.all.route_localnet=1
+  fi
 
-  # Allow sslh to use localhost as destination
-  /sbin/sysctl -q -w net.ipv4.conf.default.route_localnet=1
-  /sbin/sysctl -q -w net.ipv4.conf.all.route_localnet=1
+  TAG="-m comment --comment ${RULE_COMMENT}"
 
-  # sslh requires net admin capabilities for transparent redirect
-  set_capabilities ${TRANSPARENT_SETCAP:-ignore}
+  # This script requires root or capabilities: CAP_NET_ADMIN CAP_NET_RAW
 
   ### IPv4 ###
 
   # Drop martian packets as they would have been if route_localnet was zero
   # Packets not leaving the server aren't affected by this
-  /sbin/iptables -t raw    -A PREROUTING  ! -i lo -d 127.0.0.0/8 -j DROP ${TAG}
-  /sbin/iptables -t mangle -A POSTROUTING ! -o lo -s 127.0.0.0/8 -j DROP ${TAG}
+  eval /sbin/iptables -t raw ${OPT} PREROUTING ${TAG} \
+                      ! -i lo -d 127.0.0.0/8 -j DROP ${LOG}
+  eval /sbin/iptables -t mangle ${OPT} POSTROUTING ${TAG} \
+                      ! -o lo -s 127.0.0.0/8 -j DROP ${LOG}
 
   # Mark all connections made by sslh for special treatment
-  /sbin/iptables -t nat -A OUTPUT ${TAG} \
-    -p tcp --syn \
-    -m owner --uid-owner ${SSLH_UID} \
-    -j CONNMARK --set-xmark ${MARK_VALUE}/${MARK_MASK}
+  eval /sbin/iptables -t nat ${OPT} OUTPUT ${TAG} \
+                      -p tcp --syn \
+                      -m owner --uid-owner ${SSLH_UID} \
+                      -j CONNMARK --set-xmark ${MARK_VALUE}/${MARK_MASK} ${LOG}
 
   # Mark (ctmark -> fwmark) outgoing packets destined to sslh for rerouting
-  /sbin/iptables -t mangle -A OUTPUT ${TAG} \
-    -p tcp ! -o lo \
-    -m connmark --mark ${MARK_VALUE}/${MARK_MASK} \
-    -j CONNMARK --restore-mark --mask ${MARK_MASK}
+  eval /sbin/iptables -t mangle ${OPT} OUTPUT ${TAG} \
+                      -p tcp ! -o lo \
+                      -m connmark --mark ${MARK_VALUE}/${MARK_MASK} \
+                      -j CONNMARK --restore-mark --mask ${MARK_MASK} ${LOG}
 
-  # Configure routing for those marked packets
-  /sbin/ip -4 rule  add fwmark ${MARK_VALUE} lookup ${LOOKUP_TABLE}
-  /sbin/ip -4 route add local  0/0  dev lo   table 100
+  # Set routing for marked IPv4 packets
+  eval /sbin/ip -4 rule  ${CMD} fwmark ${MARK_VALUE} lookup ${ROUTE_TABLE} ${LOG}
+  eval /sbin/ip -4 route ${CMD} local  0/0  dev lo   table  ${ROUTE_TABLE} ${LOG}
 
   ### IPv6 ###
 
   # Drop martian packets as they would have been if route_localnet was zero
   # Packets not leaving the server aren't affected by this
-  /sbin/ip6tables -t raw    -A PREROUTING  ! -i lo -d ::1/128 -j DROP ${TAG}
-  /sbin/ip6tables -t mangle -A POSTROUTING ! -o lo -s ::1/128 -j DROP ${TAG}
+  eval /sbin/ip6tables -t raw ${OPT} PREROUTING ${TAG} \
+                       ! -i lo -d ::1/128 -j DROP ${LOG}
+  eval /sbin/ip6tables -t mangle ${OPT} POSTROUTING ${TAG} \
+                       ! -o lo -s ::1/128 -j DROP ${TAG} ${LOG}
 
   # Mark all connections made by sslh for special treatment
-  /sbin/ip6tables -t nat -A OUTPUT ${TAG} \
-    -m owner --uid-owner ${SSLH_UID} \
-    -p tcp --syn \
-    -j CONNMARK --set-xmark ${MARK_VALUE}/${MARK_MASK}
+  eval /sbin/ip6tables -t nat ${OPT} OUTPUT ${TAG} \
+                       -m owner --uid-owner ${SSLH_UID} \
+                       -p tcp --syn \
+                       -j CONNMARK --set-xmark ${MARK_VALUE}/${MARK_MASK} ${LOG}
 
   # Mark (ctmark -> fwmark) outgoing packets destined to sslh for rerouting
-  /sbin/ip6tables -t mangle -A OUTPUT ${TAG} \
-    -p tcp ! -o lo \
-    -m connmark --mark ${MARK_VALUE}/${MARK_MASK} \
-    -j CONNMARK --restore-mark --mask ${MARK_MASK}
+  eval /sbin/ip6tables -t mangle ${OPT} OUTPUT ${TAG} \
+                       -p tcp ! -o lo \
+                       -m connmark --mark ${MARK_VALUE}/${MARK_MASK} \
+                       -j CONNMARK --restore-mark --mask ${MARK_MASK} ${LOG}
 
-  # Configure routing for those marked packets
-  /sbin/ip -6 rule  add fwmark ${MARK_VALUE} lookup ${LOOKUP_TABLE}
-  /sbin/ip -6 route add local  ::/0  dev lo  table ${LOOKUP_TABLE}
+  # Set routing for marked IPv6 packets
+  eval /sbin/ip -6 rule  ${CMD} fwmark ${MARK_VALUE} lookup ${ROUTE_TABLE} ${LOG}
+  eval /sbin/ip -6 route ${CMD} local  ::/0  dev lo  table  ${ROUTE_TABLE} ${LOG}
 }
 
-stop()
-{
-  [ ${USE_TRANSPARENT} = true ] || return
-
-  /sbin/iptables-save  | /bin/grep -v ${COMMENT} | /sbin/iptables-restore
-  /sbin/ip6tables-save | /bin/grep -v ${COMMENT} | /sbin/ip6tables-restore
-
-  /sbin/ip -4 rule  del fwmark ${MARK_VALUE} lookup ${LOOKUP_TABLE} 2>/dev/null || true
-  /sbin/ip -6 rule  del fwmark ${MARK_VALUE} lookup ${LOOKUP_TABLE} 2>/dev/null || true
-  /sbin/ip -4 route del local  0/0  dev lo table ${LOOKUP_TABLE} 2>/dev/null || true
-  /sbin/ip -6 route del local  ::/0 dev lo table ${LOOKUP_TABLE} 2>/dev/null ||true
-}
-
-case "${ACTION}" in
-  start) start ;;
-  stop)  stop ;;
-  restart|reload) stop; start ;;
+case "${1:-}" in
+  start) rules start ;;
+  stop)  rules stop  ;;
+  restart|reload) rules stop; rules start ;;
   *) echo "usage: $0 start|stop|restart"; exit 1 ;;
 esac
